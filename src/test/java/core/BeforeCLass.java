9b3641332b1f7d2108e2d58fd4efae943a3f833d
Aiana Abaikanova <bahayana80@gmail.com>
2019-06-19 15:24:13 -0700

    Initial commit

---
+++ src/test/java/core/BeforeCLass.java
@@ -0,0 +1,13 @@
+package core;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target(METHOD)
+public @interface BeforeCLass {
+
+}
